4a321788c161fe9af2e069366ca0de477e4be626
Karen Coombs <librarywebchic@gmail.com>
2014-08-25 20:25:19 -0500

    Added function to get a resource by URI to the Thing class

--- src/Thing.php
+++ src/Thing.php
@@ -15,8 +15,11 @@
 
 namespace WorldCat\Discovery;
 
+use \EasyRdf_Graph;
 use \EasyRdf_Resource;
 use \EasyRdf_Format;
+use \EasyRdf_Namespace;
+use \EasyRdf_TypeMapper;
 
 /**
  * A class that represents an Intangible in Schema.org
@@ -34,5 +37,45 @@ class Thing extends EasyRdf_Resource
     {
         $name = $this->get('schema:name');
         return $name;
+    }
+    
+    public static function findByURI($uri, $returnGraph = false) {
+        EasyRdf_Namespace::set('schema', 'http://schema.org/');
+        EasyRdf_Namespace::set('owl', 'http://www.w3.org/2002/07/owl#');
+        EasyRdf_Namespace::set('rdfs', 'http://www.w3.org/2000/01/rdf-schema#');
+        EasyRdf_Namespace::set('foaf', 'http://xmlns.com/foaf/0.1/');
+        EasyRdf_Namespace::set('madsrdf', 'http://www.loc.gov/mads/rdf/v1#');
+        EasyRdf_TypeMapper::set('schema:Organization', 'WorldCat\Discovery\Organization');
+        EasyRdf_TypeMapper::set('foaf:Organization', 'WorldCat\Discovery\Organization'); // will be deprecated
+        EasyRdf_TypeMapper::set('schema:Person', 'WorldCat\Discovery\Person');
+        EasyRdf_TypeMapper::set('foaf:Person', 'WorldCat\Discovery\Person'); // will be deprecated
+        EasyRdf_TypeMapper::set('schema:Place', 'WorldCat\Discovery\Place');
+        EasyRdf_TypeMapper::set('http://dbpedia.org/ontology/Place', 'WorldCat\Discovery\Place'); // will be deprecated
+        EasyRdf_TypeMapper::set('schema:CreativeWork', 'WorldCat\Discovery\CreativeWork');
+        EasyRdf_TypeMapper::set('madsrdf:Topic', 'WorldCat\Discovery\AuthorityTopical');
+        EasyRdf_TypeMapper::set('madsrdf:Geographic', 'WorldCat\Discovery\AuthorityGeographic');
+        EasyRdf_TypeMapper::set('madsrdf:Authority', 'WorldCat\Discovery\Authority');
+    
+        $guzzleOptions = array(
+            'headers' => array(
+                'Accept' => 'application/rdf+xml'
+            )
+        );
+        
+        try {
+            $response = \Guzzle::get($uri, $guzzleOptions);
+            $graph = new EasyRdf_Graph();
+            $graph->parse($response->getBody(true));
+            if ($returnGraph){
+                return $graph;
+            } else {
+                $resource = $graph->resource($uri);
+                return $resource;
+            }
+        } catch (\Guzzle\Http\Exception\BadResponseException $error) {
+            return Error::parseError($error);
+        }
+        
+        
     }
 }
